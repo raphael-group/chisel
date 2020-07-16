ef0f92b519f8d6f886be1608d4907f72fe65e03a
Simone Zaccaria <simozacca@gmail.com>
2020-07-16 13:01:49 -0400

    Fix OSx installation

--- install_full.sh
+++ install_full.sh
@@ -14,16 +14,15 @@ esac
 # Finding whether machine is 32bit or 64bit
 case ${OS} in
     Linux)
-	VER=$(arch)
+	VER=$(uname -i)
 	case ${VER} in
-	    x86_64)    VER="x86_64";;
-	    *)         VER="x86";;
+	    x86_64)    MINICONDA="https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh";;
+	    *)         MINICONDA="https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86.sh";;
 	esac;;
     MacOSX)
-	VER=$(arch)
+	VER=$(uname -m)
 	case ${VER} in
-	    x86_64)    VER="x86_64";;
-	    *)         echo "Mac OSX 32 bit is not supported by Miniconda, please use custom installation." && exit 1;;
+	    *)         MINICONDA="https://repo.anaconda.com/miniconda/Miniconda2-latest-MacOSX-x86_64.sh";;
 	esac;;
     *)
         echo "Unknown OS ${OS}; please use manual installation." && exit 1;;
@@ -32,7 +31,7 @@ esac
 # Installing Miniconda
 CHISEL_HOME=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 cd ${CHISEL_HOME}
-curl -L https://repo.anaconda.com/miniconda/Miniconda2-latest-${OS}-${VER}.sh > miniconda.sh
+curl -L ${MINICONDA} > miniconda.sh
 rm -rf ./conda/
 bash miniconda.sh -b -f -p ./conda/
 export CONDA_HOME=${CHISEL_HOME}/conda/bin/
@@ -45,4 +44,4 @@ ${CONDA_HOME}/conda create -n chisel chisel -y
 
 # Activating CHISEL
 source ${CONDA_HOME}/activate chisel
-echo -e "\nInstallation was succesfull and CHISEL is ready!\nPlease remember to run the following command now and during every new session before using CHISEL:\n\t\tsource ${CONDA_HOME}/activate chisel\n\n"
+echo -e "\nInstallation was succesfull and CHISEL is ready!\nPlease remember to run the following command now and during every new session before using CHISEL:\n\n\t\tsource ${CONDA_HOME}/activate chisel\n\n"
