1d1fa4c55932190f02f7cc3277c77b421548c1f4
Simone Zaccaria <simozacca@gmail.com>
2020-07-17 14:11:38 -0400

    New complete demo on smaller BAM

--- demos/complete/demo-complete.sh
+++ demos/complete/demo-complete.sh
@@ -1,14 +1,17 @@
 # Demo for WGS data from a cancer patient
 : ex: set ft=markdown ;:<<'```shell' #
 
-The following CHISEL demo represents a guided example of the complete CHISEL pipeline starting from the barcoded [BAM file](https://support.10xgenomics.com/single-cell-dna/datasets/1.0.0/breast_tissue_E_2k) publicly available from 10X Genomics archive and obtained through 10X Chromium Single Cell CNV Solution for section E of a breast tumor. Simply run this file through BASH as a standard script to run the complete demo. The demo can also be considered as a guided example of a complete execution and is correspondingly commented.
+The following CHISEL demo represents a guided example of the complete CHISEL pipeline starting from an exemplary barcoded [BAM file](https://doi.org/10.5281/zenodo.3950299) publicly available. From this directory, simply run this file through BASH as a standard script to run the complete demo. The demo can also be considered as a guided example of a complete execution and is correspondingly commented.
 
 ## Requirements and set up
 
-The demo requires that CHISEL has been succesfully installed, such that the python environment called by the command `python2.7` has the required packages, and both `samtools` and `awk` are available in `${PATH}`.
+The demo requires that CHISEL has been succesfully installed with conda. If the custom installation was used, please make sure that you can succesfully run the command `chisel` as well as the required `samtools`, `bcftools`, and `awk`. The demo includes the downloading of all the required files and will terminate in <20 minutes on machine with minimum requirements satisfied.
+
+We gurantee that the running directory in the same directory of the demo and we remove previous results.
 
 ```shell
-export CHISEL_HOME="../../" # This is CHISEL home by default, update if needed
+cd $( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
+rm -rf rdr/ baf/ combo/ calls/ clones/ plots/
 :<<'```shell' # Ignore this line
 ```
 
@@ -22,21 +25,21 @@ PS4='[\t]'
 
 ## Downloading of data
 
-The demo auomatically downloads the required barcoded single-cell and matched-normal BAM files from 10X Genomics archive through the following commands in `data` folder.
+The demo auomatically downloads the required barcoded single-cell and matched-normal BAM files in `data` folder.
 
 ```shell
 # Creating data folder
 mkdir -p data
 
-# Downloading barcoded single-cell BAM of breast tumor section E
-wget -N -c http://s3-us-west-2.amazonaws.com/10x.files/samples/cell-dna/1.0.0/breast_tissue_E_2k/breast_tissue_E_2k_possorted_bam.bam -P data/
-wget -N -c http://cf.10xgenomics.com/samples/cell-dna/1.0.0/breast_tissue_E_2k/breast_tissue_E_2k_possorted_bam.bam.bai -P data/
-export TUM="data/breast_tissue_E_2k_possorted_bam.bam"
+# Downloading tumor barcoded BAM file
+curl -L 'https://zenodo.org/record/3950299/files/cells.bam?download=1' > data/cells.bam
+curl -L 'https://zenodo.org/record/3950299/files/cells.bam.bai?download=1' > data/cells.bam.bai
+export TUM="data/cells.bam"
 
-# Downloading matched-normal BAM file as section A
-wget -N -c http://s3-us-west-2.amazonaws.com/10x.files/samples/cell-dna/1.0.0/breast_tissue_A_2k/breast_tissue_A_2k_possorted_bam.bam -P data/
-wget -N -c http://cf.10xgenomics.com/samples/cell-dna/1.0.0/breast_tissue_A_2k/breast_tissue_A_2k_possorted_bam.bam.bai -P data/
-export NOR="data/breast_tissue_A_2k_possorted_bam.bam"
+# Downloading matched-normal BAM file
+curl -L 'https://zenodo.org/record/3950299/files/normal.bam?download=1' > data/normal.bam
+curl -L 'https://zenodo.org/record/3950299/files/normal.bam.bai?download=1' > data/normal.bam.bai
+export NOR="data/normal.bam"
 :<<'```shell' # Ignore this line
 ```
 
@@ -46,19 +49,18 @@ Next the corresponding reference genome is downloaded and unpacked
 export REF="data/refdata-GRCh38-2.1.0/fasta/genome.fa"
 export DIC="data/refdata-GRCh38-2.1.0/fasta/genome.dict"
 if [[ ! -f "${REF}" || ! -f "${DIC}" ]]; then
-    wget -N -c http://cf.10xgenomics.com/supp/genome/refdata-GRCh38-2.1.0.tar.gz -P data/
+    curl -L http://cf.10xgenomics.com/supp/genome/refdata-GRCh38-2.1.0.tar.gz > data/refdata-GRCh38-2.1.0.tar.gz
     tar -xzvf data/refdata-GRCh38-2.1.0.tar.gz -C data/
     rm -f data/refdata-GRCh38-2.1.0.tar.gz
 fi
 :<<'```shell' # Ignore this line
 ```
 
-Last, we download the pre-computed VCF with phased SNPs; the VCF has been computed following the reccommended instructions, using BCFtools to call germline SNPs and Eagle2 throught the Michigan Imputation Serverve with HRC panel to phase the SNPs.
+Last, we download the pre-computed list of phased germline SNPs. Note that differently from the [one](https://github.com/raphael-group/chisel-data/raw/master/demos/completeE/phased.HRC.vcf.gz) obtained through the reccommended instructions (i.e. using BCFtools to call germline SNPs and Eagle2 throught the Michigan Imputation Serverve with HRC panel to phase the SNPs) this file only contains the lables `0|1` or `1|0` for every SNP, which is the minimum requirement for CHISEL.
 
 ```shell
-wget -N -c https://github.com/raphael-group/chisel-data/raw/master/demos/completeE/phased.HRC.vcf.gz -P data/
-gzip -f -d data/phased.HRC.vcf.gz
-export PHA="data/phased.HRC.vcf"
+curl -L 'https://zenodo.org/record/3950299/files/phases.tsv?download=1' -P data/phased.tsv
+export PHA="data/phased.tsv"
 :<<'```shell' # Ignore this line
 ```
 
@@ -67,6 +69,6 @@ export PHA="data/phased.HRC.vcf"
 We now run the complete pipeline of CHISEL with the corresponding command `chisel`.
 
 ```shell
-python2.7 ${CHISEL_HOME}/bin/chisel.py -t ${TUM} -n ${NOR} -r ${REF} -l ${PHA} --seed 25
+chisel -t ${TUM} -n ${NOR} -r ${REF} -l ${PHA} --seed 12
 exit $?
 ```
